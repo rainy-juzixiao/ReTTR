26643b7993403dd2edc65a4f2695ee7cbc2f67fc
rainy-juzixiao <aTzXQR8w96I@outlook.com>
2026-05-21 09:28:14 +0800

    Move rettr/core/array_range.hpp to rettr/

--- lunar/include/rettr/array_range.hpp
+++ lunar/include/rettr/array_range.hpp
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef RETTR_CORE_ARRAY_RANGE_HPP
-#define RETTR_CORE_ARRAY_RANGE_HPP
+#ifndef RETTR_ARRAY_RANGE_HPP
+#define RETTR_ARRAY_RANGE_HPP
 #include <rettr/core/prerequisites.hpp>
 
 namespace rettr {
