8b66ef4740e03918f8449873843b09cb76d91835
rainy-juzixiao <aTzXQR8w96I@outlook.com>
2026-05-23 14:15:10 +0800

    Reformat method.hpp

--- lunar/include/rettr/method.hpp
+++ lunar/include/rettr/method.hpp
@@ -1,5 +1,5 @@
 /*
-* Copyright 2026 rainy-juzixiao
+ * Copyright 2026 rainy-juzixiao
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,50 +16,55 @@
 #ifndef RETTR_METHOD_HPP
 #define RETTR_METHOD_HPP
 
-#include <rettr/function.hpp>
-#include <rettr/parameter_info.hpp>
 #include <rettr/access_levels.hpp>
-#include <rettr/implements/metadata.hpp>
+#include <rettr/function.hpp>
 #include <rettr/implements/iterator.hpp>
+#include <rettr/implements/metadata.hpp>
+#include <rettr/parameter_info.hpp>
 #include <unordered_map>
 #include <vector>
 
 namespace rettr {
     class RETTR_API method : function {
     public:
-        using function::empty;
-        using function::arity;
-        using function::paramlists;
         using function::arg;
+        using function::arity;
+        using function::empty;
+        using function::paramlists;
         using function::operator bool;
-        using function::is_invocable;
-        using function::is_invocable_with;
-        using function::is_variadic_invocable_with;
-        using function::is_static;
-        using function::is_memfn;
-        using function::is_const;
-        using function::is_noexcept;
-        using function::is_volatile;
-        using function::is_invoke_for_lvalue;
-        using function::is_invoke_for_rvalue;
         using function::invoke;
         using function::invoke_variadic;
+        using function::is_const;
+        using function::is_invocable;
+        using function::is_invocable_with;
+        using function::is_invoke_for_lvalue;
+        using function::is_invoke_for_rvalue;
+        using function::is_memfn;
+        using function::is_noexcept;
+        using function::is_static;
+        using function::is_variadic_invocable_with;
+        using function::is_volatile;
         using function::operator();
-        using function::static_invoke;
         using function::return_type;
+        using function::static_invoke;
 
         method() noexcept = default;
+
         method(const method &) noexcept = default;
+
         method(method &&) noexcept = default;
+
         method &operator=(const method &) noexcept = default;
+
         method &operator=(method &&) noexcept = default;
+
         ~method() = default;
 
         void swap(method &right) noexcept {
             function::swap(right);
             params_.swap(right.params_);
             metadatas_.swap(right.metadatas_);
-            std::swap(name_,         right.name_);
+            std::swap(name_, right.name_);
             std::swap(access_level_, right.access_level_);
         }
 
@@ -68,8 +73,7 @@ namespace rettr {
         }
 
         rettr::type declaring_type() const noexcept {
-            return rettr::type::from_typeid(
-                function::which_belongs().remove_cvref());
+            return rettr::type::from_typeid(function::which_belongs().remove_cvref());
         }
 
         access_levels access_level() const noexcept {
@@ -77,11 +81,13 @@ namespace rettr {
         }
 
         array_range<parameter_info> parameter_infos() const noexcept {
-            return { params_.data(), params_.size() };
+            return {params_.data(), params_.size()};
         }
 
         parameter_info parameter(std::size_t idx) const noexcept {
-            if (idx >= params_.size()) return {};
+            if (idx >= params_.size()) {
+                return {};
+            }
             return params_[idx];
         }
 
@@ -108,26 +114,20 @@ namespace rettr {
         }
 
     private:
-        explicit method(function &&fn,
-                        string_view name,
-                        access_levels access_level,
-                        std::vector<parameter_info> &&params,
-                        std::unordered_map<any, rettr::metadata> &&metadata) noexcept
-            : function(std::move(fn))
-            , name_(name)
-            , access_level_(access_level)
-            , params_(std::move(params))
-            , metadatas_(std::move(metadata)) {}
+        explicit method(function &&fn, string_view name, access_levels access_level, std::vector<parameter_info> &&params,
+                        std::unordered_map<any, rettr::metadata> &&metadata) noexcept :
+            function(std::move(fn)), name_(name), access_level_(access_level), params_(std::move(params)),
+            metadatas_(std::move(metadata)) {
+        }
 
-        string_view                      name_;
-        access_levels                    access_level_{ access_levels::public_access };
-        std::vector<parameter_info>      params_;
+        string_view name_;
+        access_levels access_level_{access_levels::public_access};
+        std::vector<parameter_info> params_;
         std::unordered_map<any, rettr::metadata> metadatas_;
 
-        template<typename>
+        template <typename>
         friend class implements::method_bind;
     };
-
 }
 
 #endif
